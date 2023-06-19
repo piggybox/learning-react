f25580127d6402f6620fddde11e6987914c6f58a
Sky Yin <sky.yin@gmail.com>
2023-06-19 11:26:08 -0700

    switch to create-react project structure

---
+++ part-1/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <img src="./logo192.png" width="40px" alt="logo" />
+      <body>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ul>
+          <li>It's composable</li>
+          <li>It's declarative</li>
+          <li>It's hirable skills</li>
+          <li>It's actively maintained by skilled people</li>
+        </ul>
+      </body>
+    </div>
+  )
+}
+
+export default App
