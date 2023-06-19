f2e9e0fe1b99bf08cd8cfa771e805ade4952e3bc
Sky Yin <sky.yin@gmail.com>
2023-06-19 12:37:52 -0700

    Make the app composable

--- part-1/src/App.js
+++ part-1/src/App.js
@@ -1,19 +1,47 @@
 import "./App.css"
 
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./logo192.png" width="40px" alt="logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li> 
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer></footer>
+  )
+}
+
+function Body() {
+  return (
+    <body>
+      <h1>My awesome website in React</h1>
+      <h3>Reasons I love React</h3>
+      <ul>
+        <li>It's composable</li>
+        <li>It's declarative</li>
+        <li>It's hirable skills</li>
+        <li>It's actively maintained by skilled people</li>
+      </ul>
+    </body>
+  )
+}
+
 function App() {
   return (
-    <div>
-      <img src="./logo192.png" width="40px" alt="logo" />
-      <body>
-        <h1>My awesome website in React</h1>
-        <h3>Reasons I love React</h3>
-        <ul>
-          <li>It's composable</li>
-          <li>It's declarative</li>
-          <li>It's hirable skills</li>
-          <li>It's actively maintained by skilled people</li>
-        </ul>
-      </body>
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
     </div>
   )
 }
