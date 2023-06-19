ce094e9dbc125026b27c86cac67ee79fae8ecf8d
Sky Yin <sky.yin@gmail.com>
2023-06-19 11:05:36 -0700

    Feat: add content for project 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Learning React
+
+https://www.youtube.com/watch?v=bMknfKXIFA8&t=126s
--- project-1/index.js
+++ project-1/index.js
@@ -1,5 +1,16 @@
-const Temp = () => {
-  return <h1>test</h1>;
-};
 
-ReactDOM.render(<Temp />, document.getElementById("root"));
+const Temp = () => {
+  return <div>
+    <h1>My awesome website in React</h1>
+    <h3>Reasons I love React</h3>
+    <ol>
+      <li>It's composable</li>
+      <li>It's declarative</li>
+      <li>It's hirable skills</li>
+      <li>It's actively maintained by skilled people</li>
+    </ol>
+
+  </div>
+}
+
+ReactDOM.render(<Temp />, document.getElementById("root"))
